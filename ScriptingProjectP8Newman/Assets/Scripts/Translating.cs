f2e203c79f5bad2abd8f4e200e53613f0f00853a
Maximum4ce <727149@student.dusd.net>
2025-01-13 14:36:48 -0800

    htrdrg

---
+++ ScriptingProjectP8Newman/Assets/Scripts/Translating.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translating : MonoBehaviour
+{
+   
+   
+        public float moveSpeed = 10f;
+        public float turnSpeed =100f;
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+                transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.RightArrow))
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    
+}
+
